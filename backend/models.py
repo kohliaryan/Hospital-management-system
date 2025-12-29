f0880eefa8495d185fcd123f71f89612240bb5f1
kohliaryan <kohliaryan2004@gmail.com>
2025-12-29 22:43:37 +0530

    Adding flask security

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,22 @@
+import uuid
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
 db = SQLAlchemy()
 
-class User(db.Model):
+user_roles = db.Table("user_roles",
+                      db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+                      db.Column("role_id", db.Integer, db.ForeignKey("role.id"), primary_key=True))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean(), default=True)
+
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
+
+    roles = db.relationship("Role", secondary=user_roles,backref=db.backref("users", lazy="dynamic"))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)

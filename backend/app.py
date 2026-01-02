d918d5329c301233de425d421d46326f89c46194
kohliaryan <kohliaryan2004@gmail.com>
2026-01-02 14:14:50 +0530

    Adding CORS and fixing structure

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,11 @@ from flask_security import Security, SQLAlchemyUserDatastore
 from config import Config
 from models import db, User, Role
 from create_initial_data import create_initial_data
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
     app.config.from_object(Config)
 
     db.init_app(app)
